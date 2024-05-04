96d2a7273bab4e77cf778a7f4b2c387c4cdae35f
JW0203 <widwoo0203@gmail.com>
2024-05-04 23:47:32 +0900

    fix: board entity

--- src/entities/Board.entity.ts
+++ src/entities/Board.entity.ts
@@ -28,14 +28,12 @@ export class Board extends Timestamps {
   constructor();
   constructor(params: { title: string; content: string });
   constructor(params?: { title: string; content: string }) {
-    console.log('생성자 실행');
     super();
-    console.log(params);
-    this.title = params.title;
-    this.content = params.content;
-    // if (params) {
-    //   this.title = params.title;
-    //   this.content = params.content;
-    // }
+    // this.title = params.title;
+    // this.content = params.content;
+    if (params) {
+      this.title = params.title;
+      this.content = params.content;
+    }
   }
 }
